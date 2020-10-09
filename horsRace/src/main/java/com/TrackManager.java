e2e4a0ff2d2d14e5d727f4bf96df62dc5b16da17
AlexseyOstrovskiy <curtwonnengut@gmail.com>
2020-10-09 14:37:41 +0300

    full

--- horsRace/src/main/java/com/TrackManager.java
+++ horsRace/src/main/java/com/TrackManager.java
@@ -14,62 +14,95 @@ public class TrackManager {
     }
 
     public void run() {
-//        Rider test =  riders.get(1);
-//        System.out.println(test.getHorse());
         int circleCounter = 0;
         Rider crew1 = riders.get(0);
         Rider crew2 = riders.get(1);
         Rider crew3 = riders.get(2);
-        //while (circleCounter != 5) {
-            String step = "_1";
-            String twoStep = "_ _1";
-            System.out.println("Start!");
-           // int winNum = (int) (Math.random() * 3) + 1;
-            int winNum = 3;
+        System.out.println("Start!");
+        int counter1=1;
+        int counter2=1;
+        int counter3=1;
+        while (circleCounter != 5) {
+            String step = "*";
+            String twoStep = "*";
+            int winNum = (int) (Math.random() * 3) + 1;
             circleCounter= circleCounter + 1;
+
             if (winNum == 1) {
+                System.out.println("");
+                System.out.println("ROUND # " + circleCounter);
                 System.out.println(crew1.getName() + crew1.getAge() + crew1.getHorse());
-                for (int i = 0; i < circleCounter; i++ ) {
+                counter1=counter1+1;
+               // System.out.println("test win num 1/1 --"+winNum+" counter1 " +counter1 + "counter2 "+counter2+"counter3 "+counter3);
+                for (int i = 0; i < counter1; i++) {
                     System.out.print(twoStep);
                 }
+                System.out.println("");
                 System.out.println(crew2.getName() + crew2.getAge() + crew2.getHorse());
-                for (int i = 0; i < circleCounter; ) {
+                for (int i = 0; i < counter2; i++) {
                     System.out.print(step);
                 }
+                System.out.println("");
                 System.out.println(crew3.getName() + crew3.getAge() + crew3.getHorse());
-                for (int i = 0; i < circleCounter; ) {
+                for (int i = 0; i < counter3; i++) {
                     System.out.print(step);
                 }
             }
-            else if (winNum == 2) {
+             if (winNum == 2) {
+                System.out.println("");
+                System.out.println("ROUND # " + circleCounter);
                 System.out.println(crew1.getName() + crew1.getAge() + crew1.getHorse());
-                for (int i = 0; i < circleCounter; ) {
+                for (int i = 0; i < counter1; i++) {
                     System.out.print(step);
                 }
+                System.out.println("");
                 System.out.println(crew2.getName() + crew2.getAge() + crew2.getHorse());
-                for (int i = 0; i < circleCounter; ) {
+                counter2=counter2+1;
+                // System.out.println("test win num 2/2--"+winNum+" counter1 " +counter1 + "counter2 "+counter2+"counter3 "+counter3);
+                for (int i = 0; i < counter2; i++) {
                     System.out.print(twoStep);
                 }
+                System.out.println("");
                 System.out.println(crew3.getName() + crew3.getAge() + crew3.getHorse());
-                for (int i = 0; i < circleCounter; ) {
+                for (int i = 0; i < counter3; i++) {
                     System.out.print(step);
                 }
-            } else {
-                System.out.println(crew1.getName() + crew1.getAge() + crew1.getHorse());
-                for (int i = 0; i < circleCounter; ) {
-                    System.out.print(step);
-                }
-                System.out.println(crew2.getName() + crew2.getAge() + crew2.getHorse());
-                for (int i = 0; i < circleCounter; ) {
-                    System.out.print(step);
-                }
-                System.out.println(crew3.getName() + crew3.getAge() + crew3.getHorse());
-                for (int i = 0; i < circleCounter; ) {
-                    System.out.print(twoStep);
-                }
-
             }
+             if(winNum == 3) {
+                System.out.println("");
+                System.out.println("ROUND # " + circleCounter);
+                System.out.println(crew1.getName() + crew1.getAge() + crew1.getHorse());
 
+                for (int i = 0; i < counter1; i++) {
+                    System.out.print(step);
+                }
+                System.out.println("");
+                System.out.println(crew2.getName() + crew2.getAge() + crew2.getHorse());
+                    for (int i = 0; i < counter2; i++) {
+                        System.out.print(step);
+                    }
+                System.out.println("");
+                System.out.println(crew3.getName() + crew3.getAge() + crew3.getHorse());
+                counter3=counter3+1;
+                // System.out.println("test win num 3/3--"+winNum+" counter1 " +counter1 + "counter2 "+counter2+"counter3 "+counter3);
+                        for (int i = 0; i < counter3; i++) {
+                            System.out.print(twoStep);
+                        }
+            }
+            System.out.println("");
         }
-   // }
+        System.out.println("");
+        System.out.println("FINISH!!!");
+        String winner;
+        if(counter1>counter2 & counter1>counter3){
+             winner = (crew1.getName()+" "+ crew1.getAge()+" "+crew1.getHorse());
+        }
+        else if(counter2>counter3){
+             winner = (crew2.getName()+" "+crew2.getAge()+" "+crew2.getHorse());
+        }
+        else {
+            winner = (crew3.getName()+" "+crew3.getAge()+" "+crew3.getHorse());
+        }
+        System.out.println("Winner: "+winner );
+    }
 }
